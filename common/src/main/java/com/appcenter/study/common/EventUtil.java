2ba8b4448f815721dd1bcc8e3f88c24b546f656b
gyuri00 <skarbfl0802@gmail.com>
2025-07-10 21:13:15 +0900

    fix: Kafka 메시지 수신 방식 String으로 변경 및 유틸 클래스 추가해서 역직렬화 오류 해결

---
+++ common/src/main/java/com/appcenter/study/common/EventUtil.java
@@ -0,0 +1,19 @@
+package com.appcenter.study.common;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class EventUtil {
+    public static <T> T mappingMessageToClass(String message, Class<T> classType) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            return objectMapper.readValue(message, classType);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
+}
